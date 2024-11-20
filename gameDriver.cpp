1947e902ba860f708c69753d39fb65c9cc05dd21
Sergio Bocci <powwerie@gmail.com>
2024-11-19 20:26:28 -0700

    board messed up

--- gameDriver.cpp
+++ gameDriver.cpp
@@ -11,7 +11,8 @@
 using namespace std;
 
 // function to print the menu for each turn
-void printMenu(){
+void printMenu()
+{
     cout << "Main Menu: Select an option to continue" << endl;
     cout << "1. Check Player Progress (1)" << endl;
     cout << "2. Review Character (2)" << endl;
@@ -23,7 +24,8 @@ void printMenu(){
 }
 
 // split function for reading from txt files
-int split(string input_string, char separator, string arr[], const int ARR_SIZE){
+int split(string input_string, char separator, string arr[], const int ARR_SIZE)
+{
     int lastDelimiter = 0;
     int elements = 0;
     for (unsigned int i = 0; i < input_string.length(); i++)
@@ -48,10 +50,12 @@ int split(string input_string, char separator, string arr[], const int ARR_SIZE)
     return elements;
 }
 
-int main(){
+int main()
+{
 
     // stores the attributes of each character option
-    struct characters{
+    struct characters
+    {
         string name;
         int age;
         int strength;
@@ -61,7 +65,8 @@ int main(){
     };
 
     // stores the attributes of each advisor option
-    struct advisors{
+    struct advisors
+    {
         string name;
         string power;
         string description;
@@ -77,13 +82,15 @@ int main(){
     string characterLine;
     ifstream charactersFile("characters.txt");
     bool firstLine = true;
-    if (!charactersFile.is_open()){
+    if (!charactersFile.is_open())
+    {
         cout << "Could not open file." << endl;
         return -1;
     }
 
     // loop that populates the charactersVector from the character file
-    while (getline(charactersFile, characterLine)){
+    while (getline(charactersFile, characterLine))
+    {
         if (!firstLine)
         {
             split(characterLine, '|', charactersString, 6);
@@ -126,17 +133,21 @@ int main(){
     string advisorLine;
     firstLine = true; // sets first line back to true for the new file
     ifstream advisorsFile("advisors.txt");
-    if (!advisorsFile.is_open()){
+    if (!advisorsFile.is_open())
+    {
         cout << "Could not open file." << endl;
         return -1;
     }
 
     // loop that populates the advisorsVector from the advisors file
-    while (getline(advisorsFile, advisorLine)){
-        if (!firstLine){
+    while (getline(advisorsFile, advisorLine))
+    {
+        if (!firstLine)
+        {
             split(advisorLine, '|', advisorsString, 3);
             advisors tempAdvisor;
-            for (unsigned int j = 0; j < 3; j++){
+            for (unsigned int j = 0; j < 3; j++)
+            {
                 switch (j)
                 {
                 case 0:
@@ -178,7 +189,8 @@ int main(){
     cout << player1.getName() << ", choose your character: Apollo (1), Mane (2), Elsa (3), Zuri (4), Roary (5), or name your own character with random attributes(6)" << endl;
     cout << endl; // skip a line
     // print the attributes of each character in the characters vector
-    for (unsigned int i = 0; i < charactersVector.size(); i++){
+    for (unsigned int i = 0; i < charactersVector.size(); i++)
+    {
         cout << "Name: " << charactersVector[i].name
              << ", Age: " << charactersVector[i].age
              << ", Strength: " << charactersVector[i].strength
@@ -188,9 +200,12 @@ int main(){
              << endl;
     }
 
-    while (cin >> characterChoice){
-        if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5' || characterChoice == '6'){
-            switch (characterChoice){
+    while (cin >> characterChoice)
+    {
+        if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5' || characterChoice == '6')
+        {
+            switch (characterChoice)
+            {
             case '1':
                 player1.setName(charactersVector[0].name);
                 player1.setAge(charactersVector[0].age);
@@ -198,7 +213,8 @@ int main(){
                 player1.setWisdom(charactersVector[0].wisdom);
                 player1.setStrength(charactersVector[0].strength);
                 player1.setPridePoints(charactersVector[0].pridePoints);
-                for (unsigned int i = 0; i < charactersVector.size() - 1; i++){
+                for (unsigned int i = 0; i < charactersVector.size() - 1; i++)
+                {
                     charactersVector[i] = charactersVector[i + 1];
                 }
                 charactersVector.pop_back();
@@ -210,7 +226,8 @@ int main(){
                 player1.setWisdom(charactersVector[1].wisdom);
                 player1.setStrength(charactersVector[1].strength);
                 player1.setPridePoints(charactersVector[1].pridePoints);
-                for (unsigned int i = 1; i < charactersVector.size() - 1; i++){
+                for (unsigned int i = 1; i < charactersVector.size() - 1; i++)
+                {
                     charactersVector[i] = charactersVector[i + 1];
                 }
                 charactersVector.pop_back();
@@ -222,7 +239,8 @@ int main(){
                 player1.setWisdom(charactersVector[2].wisdom);
                 player1.setStrength(charactersVector[2].strength);
                 player1.setPridePoints(charactersVector[2].pridePoints);
-                for (unsigned int i = 2; i < charactersVector.size() - 1; i++){
+                for (unsigned int i = 2; i < charactersVector.size() - 1; i++)
+                {
                     charactersVector[i] = charactersVector[i + 1];
                 }
                 charactersVector.pop_back();
@@ -234,7 +252,8 @@ int main(){
                 player1.setWisdom(charactersVector[3].wisdom);
                 player1.setStrength(charactersVector[3].strength);
                 player1.setPridePoints(charactersVector[3].pridePoints);
-                for (unsigned int i = 3; i < charactersVector.size() - 1; i++){
+                for (unsigned int i = 3; i < charactersVector.size() - 1; i++)
+                {
                     charactersVector[i] = charactersVector[i + 1];
                 }
                 charactersVector.pop_back();
@@ -246,7 +265,8 @@ int main(){
                 player1.setWisdom(charactersVector[4].wisdom);
                 player1.setStrength(charactersVector[4].strength);
                 player1.setPridePoints(charactersVector[4].pridePoints);
-                for (unsigned int i = 4; i < charactersVector.size() - 1; i++){
+                for (unsigned int i = 4; i < charactersVector.size() - 1; i++)
+                {
                     charactersVector[i] = charactersVector[i + 1];
                 }
                 charactersVector.pop_back();
@@ -274,13 +294,15 @@ int main(){
 
     cout << endl; // skip a line
     cout << player2.getName() << ", choose your character: ";
-    for (unsigned int i = 0; i < charactersVector.size(); i++){
+    for (unsigned int i = 0; i < charactersVector.size(); i++)
+    {
         cout << charactersVector[i].name << " (" << i + 1 << "), ";
     }
     cout << "or name your own character with random attributes(6)" << endl;
     cout << endl; // skip a line
     // print the attributes of each character in the characters vector
-    for (unsigned int i = 0; i < charactersVector.size(); i++){
+    for (unsigned int i = 0; i < charactersVector.size(); i++)
+    {
         cout << "Name: " << charactersVector[i].name
              << ", Age: " << charactersVector[i].age
              << ", Strength: " << charactersVector[i].strength
@@ -290,9 +312,12 @@ int main(){
              << endl;
     }
 
-    while (cin >> characterChoice){
-        if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5' || characterChoice == '6'){
-            switch (characterChoice){
+    while (cin >> characterChoice)
+    {
+        if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5' || characterChoice == '6')
+        {
+            switch (characterChoice)
+            {
             case '1':
                 player2.setName(charactersVector[0].name);
                 player2.setAge(charactersVector[0].age);
@@ -359,92 +384,116 @@ int main(){
     char choice;
     cout << endl; // skip a line
     cout << player1.getName() << ", would you like to go to the pridelands (1) or begin Cub Training (2)?" << endl;
-    while (cin >> choice){
-        if (choice == '1'){
+    while (cin >> choice)
+    {
+        if (choice == '1')
+        {
             player1.setPridelands(true);
             player1.toPrideLands();
             break;
-        }else if (choice == '2'){
+        }
+        else if (choice == '2')
+        {
             player1.setPridelands(false);
             player1.trainCub();
             cout << endl; // skip a line
             cout << player1.getName() << ", choose your advisor: Rafiki (1), Nala (2), Sarabi (3), Zazu (4), Sarafina (5)" << endl;
             cout << endl; // skip a line
-            for (unsigned int i = 0; i < advisorsVector.size(); i++){
+            for (unsigned int i = 0; i < advisorsVector.size(); i++)
+            {
                 cout << "Name: " << advisorsVector[i].name << ", Power: " << advisorsVector[i].power << ", Description: " << advisorsVector[i].description << endl;
             }
-            while (cin >> characterChoice){
-                if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5'){
-                    switch (characterChoice){
-                        case '1':
-                            player1.setAdvisor(advisorsVector[0].name);
-                            break;
-                        case '2':
-                            player1.setAdvisor(advisorsVector[1].name);
-                            break;
-                        case '3':
-                            player1.setAdvisor(advisorsVector[2].name);
-                            break;
-                        case '4':
-                            player1.setAdvisor(advisorsVector[3].name);
-                            break;
-                        case '5':
-                            player1.setAdvisor(advisorsVector[4].name);
-                            break;
+            while (cin >> characterChoice)
+            {
+                if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5')
+                {
+                    switch (characterChoice)
+                    {
+                    case '1':
+                        player1.setAdvisor(advisorsVector[0].name);
+                        break;
+                    case '2':
+                        player1.setAdvisor(advisorsVector[1].name);
+                        break;
+                    case '3':
+                        player1.setAdvisor(advisorsVector[2].name);
+                        break;
+                    case '4':
+                        player1.setAdvisor(advisorsVector[3].name);
+                        break;
+                    case '5':
+                        player1.setAdvisor(advisorsVector[4].name);
+                        break;
                     }
                     break;
-                }else{
+                }
+                else
+                {
                     cout << "Invalid Input. Please enter a valid option." << endl;
                 }
             }
             break;
-        }else{
+        }
+        else
+        {
             cout << "Invalid Input. Please enter a valid option." << endl;
         }
     }
 
     cout << endl; // skip a line
     cout << player2.getName() << ", would you like to go to the pridelands (1) or begin Cub Training (2)?" << endl;
-    while (cin >> choice){
-        if (choice == '1'){
+    while (cin >> choice)
+    {
+        if (choice == '1')
+        {
             player2.setPridelands(true);
             player2.toPrideLands();
             break;
-        }else if (choice == '2'){
+        }
+        else if (choice == '2')
+        {
             player2.setPridelands(false);
             player2.trainCub();
             cout << endl; // skip a line
             cout << player2.getName() << ", choose your advisor: Rafiki (1), Nala (2), Sarabi (3), Zazu (4), Sarafina (5)" << endl;
             cout << endl; // skip a line
-            for (unsigned int i = 0; i < advisorsVector.size(); i++){
+            for (unsigned int i = 0; i < advisorsVector.size(); i++)
+            {
                 cout << "Name: " << advisorsVector[i].name << ", Power: " << advisorsVector[i].power << ", Description: " << advisorsVector[i].description << endl;
             }
-            while (cin >> characterChoice){
-                if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5'){
-                    switch (characterChoice){
-                        case '1':
-                            player2.setAdvisor(advisorsVector[0].name);
-                            break;
-                        case '2':
-                            player2.setAdvisor(advisorsVector[1].name);
-                            break;
-                        case '3':
-                            player2.setAdvisor(advisorsVector[2].name);
-                            break;
-                        case '4':
-                            player2.setAdvisor(advisorsVector[3].name);
-                            break;
-                        case '5':
-                            player2.setAdvisor(advisorsVector[4].name);
-                            break;
+            while (cin >> characterChoice)
+            {
+                if (characterChoice == '1' || characterChoice == '2' || characterChoice == '3' || characterChoice == '4' || characterChoice == '5')
+                {
+                    switch (characterChoice)
+                    {
+                    case '1':
+                        player2.setAdvisor(advisorsVector[0].name);
+                        break;
+                    case '2':
+                        player2.setAdvisor(advisorsVector[1].name);
+                        break;
+                    case '3':
+                        player2.setAdvisor(advisorsVector[2].name);
+                        break;
+                    case '4':
+                        player2.setAdvisor(advisorsVector[3].name);
+                        break;
+                    case '5':
+                        player2.setAdvisor(advisorsVector[4].name);
+                        break;
                     }
                     break;
-                }else{
+                }
+                else
+                {
                     cout << "Invalid Input. Please enter a valid option." << endl;
                 }
             }
             break;
-        }else{
+        }
+        else
+        {
             cout << "Invalid Input. Please enter a valid option." << endl;
         }
     }
@@ -453,37 +502,85 @@ int main(){
     // initialize both boards and display them
     Board testBoard(1);
     testBoard.initializeBoard();
-    testBoard.displayBoard();
 
-cout << endl; // skip a line
-cout << player1.getName() << "'s turn." << endl;
-cout << endl; // skip a line
-char menuChoice;
-printMenu();
-while (cin >> menuChoice){
-    if (menuChoice == '1' || menuChoice == '2' || menuChoice == '3' || menuChoice == '4' || menuChoice == '5'){
-        switch (menuChoice){
-            case '1':
-                
-                break;
-            case '2':
-                
-                break;
-            case '3':
-                
-                break;
-            case '4':
-                
-                break;
-            case '5':
-                
-                break;
+    cout << endl; // skip a line
+    cout << endl; // skip a line
+    char menuChoice;
+    int turnCount = 0;
+    while (testBoard.getPlayerPosition(1) != 52 || testBoard.getPlayerPosition(2) != 52)
+    {   
+        cout<<endl;
+        testBoard.displayBoard();
+        cout<<endl;
+        if(turnCount % 2 == 0){
+            cout << player1.getName() << "'s turn." << endl;
+        }else{
+            cout << player2.getName() << "'s turn." << endl;
         }
-        break;
-    }else{
-        cout << "Invalid Input. Please enter a valid option." << endl;
+        printMenu();
+        while (cin >> menuChoice)
+        {
+            if (turnCount % 2 == 0)
+            {
+                if (menuChoice == '1' || menuChoice == '2' || menuChoice == '3' || menuChoice == '4' || menuChoice == '5')
+                {
+                    switch (menuChoice)
+                    {
+                    case '1':
+
+                        break;
+                    case '2':
+
+                        break;
+                    case '3':
+
+                        break;
+                    case '4':
+
+                        break;
+                    case '5':
+
+                        break;
+                    }
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid Input. Please enter a valid option." << endl;
+                }
+            }
+            else
+            {
+                if (menuChoice == '1' || menuChoice == '2' || menuChoice == '3' || menuChoice == '4' || menuChoice == '5')
+                {
+                    switch (menuChoice)
+                    {
+                    case '1':
+
+                        break;
+                    case '2':
+
+                        break;
+                    case '3':
+
+                        break;
+                    case '4':
+
+                        break;
+                    case '5':
+
+                        break;
+                    }
+                    break;
+                }
+                else
+                {
+                    cout << "Invalid Input. Please enter a valid option." << endl;
+                }
+            }
+        }
+        turnCount++;
     }
-}
 
     // testBoard.movePlayer(1, 2);
     // testBoard.displayBoard();
